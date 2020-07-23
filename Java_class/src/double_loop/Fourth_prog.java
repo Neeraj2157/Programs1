4fb3e1bbb5ca1fc7503bc88925c4da48b6d020f0
HP <HP@LAPTOP-4COGPLME>
2020-07-23 14:52:45 +0530

    Double loop 3 Programs

---
+++ Java_class/src/double_loop/Fourth_prog.java
@@ -0,0 +1,13 @@
+package double_loop;
+
+public class Fourth_prog 
+{
+	
+
+		public static void main(String[] args)
+		{
+			
+
+
+		}
+}
---
+++ Java_class/src/double_loop/Third_prog.java
@@ -0,0 +1,19 @@
+package double_loop;
+
+public class Third_prog
+{
+	public static void main(String[] args)
+	{
+		int i,j;
+		for(i=5;i>0;i--)
+		{
+			for(j=i;j>0;j--)
+			{
+				System.out.print("*");
+			}
+		System.out.println();
+		}
+
+	}
+
+}

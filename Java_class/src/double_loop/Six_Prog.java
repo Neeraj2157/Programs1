8491a8991be86973c6705c5b8a79dbc484ba71dc
HP <HP@LAPTOP-4COGPLME>
2020-08-02 10:03:18 +0530

    final commit

--- Java_class/src/double_loop/Six_Prog.java
+++ Java_class/src/double_loop/Six_Prog.java
@@ -4,17 +4,18 @@ public class Six_Prog
 {
 	public static void main(String[] args)
 	{
-	{        
-		int x=1;
-		for(int i=1;i<=4;i++){
-			for(int j=1;j<=i;j++)
-			{
-				System.out.print(x+" ");            
-				x++;
-			}
+		{        
+			int x=1;
+			for(int i=1;i<=4;i++)
+				{
+					for(int j=1;j<=i;j++)
+						{
+							System.out.print(x+" ");            
+							x++;
+						}
 			System.out.println();
+				}
 		}
-}
-}
+	}
 
 }
